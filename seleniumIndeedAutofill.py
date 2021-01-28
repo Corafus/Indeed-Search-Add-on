df3dfc22aacf7e45cfddfef26ee346f2a8b89db4
Corafus <36778688+Corafus@users.noreply.github.com>
2021-01-28 11:38:52 -0500

    Loop Update
    Added loops for all columns in the excel file.
    
    Added time module so that the process can be spectated more easily.
    
    Changed some variable names in all of the for loop initiations.
    
    The ranges could stand to link directly to the length of the corresponding column.

--- seleniumIndeedAutofill.py
+++ seleniumIndeedAutofill.py
@@ -1,25 +1,51 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import openpyxl, time
+wb = openpyxl.load_workbook('jobSearchTerms.xlsx')
+sheet = wb['Sheet1']
 browser = webdriver.Chrome()
 browser.get('https://www.indeed.com/advanced_search?q=&l=')
 
 
+for Exact_Phrases in range(2,8):
+    #this finds the field for exact phrase, and then enters the text 'paid training'.
+    exactPhraseElem = browser.find_element_by_id('as_phr')
 
-#this finds the field for exact phrase, and then enters the text 'paid training'.
-exactPhraseElem = browser.find_element_by_id('as_phr')
-exactPhraseElem.send_keys('paid training')
-
-
-#this finds the field for terms included in the title of the job, and then enters the text 'welder'.
-includedInTitleElem = browser.find_element_by_id('as_ttl')
-includedInTitleElem.send_keys('welder')
-
-#this finds the field for location.
-includedInTitleElem = browser.find_element_by_id('where')
-includedInTitleElem.send_keys('portland, OR')
+    for j in range(25):
+        exactPhraseElem.send_keys(Keys.BACK_SPACE)
 
 
+    exactPhraseElem.send_keys(sheet['B' + str(Exact_Phrases)].value)
+    time.sleep(2)
 
-#I think this just presses the submit key. The exactPhraseElem part isn't that important.
-exactPhraseElem.submit()
+    for Job_Titles in range(2,7):
+        #this finds the field for terms included in the title of the job, and then enters the text 'welder'.
+        includedInTitleElem = browser.find_element_by_id('as_ttl')
+
+        for k in range(25):
+            includedInTitleElem.send_keys(Keys.BACK_SPACE)
+
+        includedInTitleElem.send_keys(sheet['A' + str(Job_Titles)].value)
+        time.sleep(2)
+
+
+        for Job_Locations in range(2,9):
+            #this finds the field for location.
+            jobLocationElem = browser.find_element_by_id('where')
+
+            for l in range(25):
+                jobLocationElem.send_keys(Keys.BACK_SPACE)
+
+            jobLocationElem.send_keys(sheet['C' + str(Job_Locations)].value)
+            time.sleep(2)
+
+
+
+            #I think this just presses the submit key. The exactPhraseElem part isn't that important.
+            time.sleep(3)
+            jobLocationElem.submit()
+            time.sleep(3)
+            browser.back()
+
 
 
